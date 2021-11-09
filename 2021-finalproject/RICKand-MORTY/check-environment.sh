b642a5681ffdb8144dcbade6ede10e3dfad69cb7
Frank <handsomeqwk@163.comi>
2021-11-09 22:56:39 +0800

    2021 finial project

---
+++ 2021-finalproject/RICKand-MORTY/check-environment.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#check the environment variable
+echo "[Check] environment variable..."
+if [ -z $ROS_MASTER_URI ];then
+	echo"not exists"
+else
+	echo "OK!"
+fi
+#check ping
+echo "[Check] ping address...   "
+address=$(echo $ROS_MASTER_URI | awk -F '//' '{print $2}' |awk -F ':' '{print $1}')
+ping -c 3 -q $address >output
+checkping=$(echo $?)
+if [ $checkping -eq 0 ]
+then
+	echo "OK!"
+else
+	echo "fail!"
+fi
+rm output
+
+#check address validation
+echo "[Check] address validation..."
+area=$(echo $ROS_MASTER_URI | awk -F '//' '{print $2}' |awk -F ':' '{print $1}' |awk -F '.' '{print NF}')
+if [ $area -eq 4 ];
+then
+	echo "OK!"
+else 
+	echo "Fail!"
+fi
+
+
+
---
+++ 2021-finalproject/RICKand-MORTY/git.md
@@ -0,0 +1,9 @@
+第一题:从暂存区回退到工作区
+用git reset filename回退到工作区或者用git checkout -- <文件>丢弃工作区的改动
+
+第二题：回退版本
+git reset HEAD^或者git restore <文件>
+
+第三题:
+1.git merge
+2.用git rebase
