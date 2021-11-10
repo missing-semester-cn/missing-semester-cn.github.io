5e41855003984b8ce68a8880bc84ec9d832e5a54
zhuo_mu_niao <1208956851@qq.com>
2021-11-10 18:29:28 +0800

    fix my work

--- 2021-finalproject/zhuo-mu-niao/check-environment.sh
+++ 2021-finalproject/zhuo-mu-niao/check-environment.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 echo "test"
+echo "[Check] environment variable..."
+if [ -z $ROS_MASTER_URI ];
+then
+	echo"Failed"
+else
+	echo "Pass"
+fi
+
+echo "[Check] ping address..."
+uri=$(echo $ROS_MASTERUR_URI | awk '/http:\d+\.\d+\.\d+\.\d+\:')
+if [ -z $uri ];
+then
+	echo "Pass"
+else
+	echo "Error"
+fi
--- 2021-finalproject/zhuo-mu-niao/gitwork.md
+++ 2021-finalproject/zhuo-mu-niao/gitwork.md
@@ -1,6 +1,6 @@
 ![work1](./image/1.jpg)
 ![work2](./image/2.jpg)
 ![work3](./image/3.jpg)
-1
-2
-3
+work1
+work2
+work3
